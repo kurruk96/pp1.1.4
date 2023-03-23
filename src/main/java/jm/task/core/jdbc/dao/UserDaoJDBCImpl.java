0b3124999c034ae2c665b6ce973ee90ed71d84eb
Noteburg <kurruk72@gmail.com>
2023-03-23 21:45:18 +0300

    Исправление ошибок

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,59 +8,162 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
+        Connection connection = null;
+        Statement statement = null;
         String sql = "CREATE TABLE IF NOT EXISTS Users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, age TINYINT);";
-        try (Statement statement = connection.createStatement()) {
+        try {
+            connection = Util.getConnection();
+            statement = connection.createStatement();
             statement.executeUpdate(sql);
+            connection.commit();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
     }
 
     public void dropUsersTable() {
+        Connection connection = null;
+        Statement statement = null;
         String sql = "DROP TABLE IF EXISTS Users;";
-        try (Statement statement = connection.createStatement()) {
+        try {
+            connection = Util.getConnection();
+            statement = connection.createStatement();
             statement.executeUpdate(sql);
+            connection.commit();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
         String sql = "INSERT INTO Users (name, lastname, age) VALUES (?, ?, ?);";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
+        try {
+            connection = Util.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
 
             preparedStatement.executeUpdate();
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public void removeUserById(long id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
         String sql = "DELETE FROM Users WHERE id=?;";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try {
+            connection = Util.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setLong(1, id);
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public List<User> getAllUsers() {
+        Connection connection = null;
+        Statement statement = null;
         List<User> usersList = new ArrayList<>();
 
         String sql = "SELECT id, name, lastname, age FROM Users;";
 
-        try (Statement statement = connection.createStatement()){
+        try {
+            connection = Util.getConnection();
+            statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
 
             while (resultSet.next()) {
@@ -71,18 +174,64 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(resultSet.getByte("age"));
                 usersList.add(user);
             }
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return usersList;
     }
 
     public void cleanUsersTable() {
+        Connection connection = null;
+        Statement statement = null;
         String sql = "DELETE FROM Users;";
-        try (Statement statement = connection.createStatement()) {
+        try {
+            connection = Util.getConnection();
+            statement = connection.createStatement();
             statement.executeUpdate(sql);
+            connection.commit();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
